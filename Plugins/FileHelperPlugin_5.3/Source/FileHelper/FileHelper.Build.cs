d6c42822f6b048b48d99223c900b22c5a5ba0676
SornoaYan <2457855843@qq.com>
2025-08-14 19:20:00 +0800

    test commit

---
+++ Plugins/FileHelperPlugin_5.3/Source/FileHelper/FileHelper.Build.cs
@@ -0,0 +1,32 @@
+// Copyright 2025 RLoris
+
+using UnrealBuildTool;
+
+public class FileHelper : ModuleRules
+{
+	public FileHelper(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+        PrecompileForTargets = PrecompileTargetsType.Any;
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"Engine",
+				"JsonUtilities",
+				"Json"
+			}
+		);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"ImageWriteQueue",
+				"Slate",
+				"SlateCore"
+			}
+		);
+	}
+}
